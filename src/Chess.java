5f776b07c897e9ff3e19fad4c44ec6a101511878
lucasfmerino <lucasfmerino@gmail.com>
2024-01-01 23:11:48 -0300

    feat(model): Position class added.

---
+++ src/Chess.java
@@ -0,0 +1,11 @@
+import model.boardgame.Position;
+
+public class Chess {
+
+    public static void main(String[] args) {
+
+        Position position = new Position(3, 5);
+        System.out.println(position);
+
+    }
+}
