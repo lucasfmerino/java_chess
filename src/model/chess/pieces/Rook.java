f851ccc9f6fab36c3835a82adae212375b879413
lucasfmerino <lucasfmerino@gmail.com>
2024-01-02 19:08:42 -0300

    feat(chess/pieces): Rook piece and King piece added.

---
+++ src/model/chess/pieces/Rook.java
@@ -0,0 +1,17 @@
+package model.chess.pieces;
+
+import model.boardgame.Board;
+import model.chess.ChessPiece;
+import model.chess.Color;
+
+public class Rook extends ChessPiece {
+    
+    public Rook (Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
