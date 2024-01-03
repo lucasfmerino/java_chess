5f1c600bd99a4275c324771a98258635301e4902
lucasfmerino <lucasfmerino@gmail.com>
2024-01-03 10:23:51 -0300

    feat(chess): Implemented the ChessPosition class.

---
+++ src/model/chess/ChessException.java
@@ -0,0 +1,9 @@
+package model.chess;
+
+public class ChessException extends RuntimeException {
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+    
+}
--- src/model/chess/ChessMatch.java
+++ src/model/chess/ChessMatch.java
@@ -1,12 +1,11 @@
 package model.chess;
 
 import model.boardgame.Board;
-import model.boardgame.Position;
 import model.chess.pieces.King;
 import model.chess.pieces.Rook;
 
 public class ChessMatch {
-    
+
     private Board board;
 
     public ChessMatch() {
@@ -16,19 +15,23 @@ public class ChessMatch {
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i=0; i<board.getRows(); i++) {
-            for (int j=0; j<board.getColumns(); j++) {
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
-        
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+
     }
 
 }
